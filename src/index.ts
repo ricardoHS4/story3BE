a38d5e2716753bc60453be6c33f7d7c5dd886633
Rick <ricardohs@ricardohs.com>
2024-01-02 01:29:58 -0600

    v3 ready

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ import * as logger from "firebase-functions/logger";
 import OpenAI from "openai";
 import axios from "axios";
 
+const gptModel = "gpt-3.5-turbo-1106";
+
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
@@ -27,7 +29,7 @@ export const gptJsonMode = onRequest(
     });
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
+      model: gptModel,
       response_format: {type: "json_object"},
       messages: [
         {
@@ -56,7 +58,7 @@ export const gptNormalMode = onRequest(
     });
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
+      model: gptModel,
       messages: [
         {
           role: "user",
@@ -130,7 +132,7 @@ export const generateStory = onRequest(
     }
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
+      model: gptModel,
       response_format: {type: "json_object"},
       messages: [
         {
@@ -190,6 +192,7 @@ export const uploadStory = onRequest(
   }
 );
 
+// This V2 ensures number of twists, deep level and proper JSON key naming
 export const generateStoryV2 = functions
   .runWith({
     timeoutSeconds: 300, // Set the timeout to 5 minutes (300 seconds)
@@ -221,11 +224,13 @@ export const generateStoryV2 = functions
           "There is some missing parameter, please include" +
           "genres, topics, childs and deep",
       });
+      return;
     }
 
     let initialMessage = `
     Consider a data model named 'twist' which represent a fraction of a story and only consist of a parameter 'title' of no more than 80 chars and a parameter 'body' of no more than 1200 chars.
-    Please generate a twist containing the exposition of a story of the following genres: ${genres}
+    I need to create an interactive story where readers will be able to choose how to continue the narrative. The main objective of each twist is to convince the reader to keep reading.
+    Please generate a twist containing the exposition for a story of the following genres: ${genres}
     And the following topics: ${topics}
     I need you todeliver a JSON that has a key '0' containing the generated twist.`;
 
@@ -246,7 +251,7 @@ export const generateStoryV2 = functions
     ];
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
+      model: gptModel,
       response_format: {type: "json_object"},
       messages: initialConv,
     });
@@ -292,7 +297,7 @@ I need you todeliver a JSON that has the keys ${keysString}, each containing a t
       console.log("Aqui la conv: ", newConv);
 
       const newCompletion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo-1106",
+        model: gptModel,
         response_format: {type: "json_object"},
         messages: newConv,
       });
@@ -310,9 +315,188 @@ I need you todeliver a JSON that has the keys ${keysString}, each containing a t
       if (currentDeep < maxDeep - 1) {
         const promiseArray = [];
         for (let x = 0; x < childs; x++) {
+          let newChilds = 2;
+          if (currentDeep > 3) {
+            newChilds = 1;
+          }
           promiseArray.push(
             generateTwists(
-              childs,
+              newChilds,
+              maxDeep,
+              currentDeep + 1,
+              currentTwist + x.toString(),
+              finalConv
+            )
+          );
+        }
+        await Promise.all(promiseArray);
+      }
+    }
+  });
+
+// This V3 introduces Rising Action, Climax, Falling Action and Resolution for the story
+export const generateStoryV3 = functions
+  .runWith({
+    timeoutSeconds: 540,
+    secrets: ["OPENAI_API_KEY"],
+  })
+  .https.onRequest(async (request, response) => {
+    // Your function logic here
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const body = request.body;
+    const genres: string | undefined = body.genres;
+    const topics: string | undefined = body.topics;
+    const childs: number | undefined = body.childs;
+    const deep: number = body.deep ?? 0;
+    const extra: string | undefined = body.extra;
+
+    let storyJson = {};
+    let gptCalls = 0;
+
+    if (
+      genres == undefined ||
+      topics == undefined ||
+      childs == undefined ||
+      deep == 0
+    ) {
+      response.status(400).json({
+        message:
+          "There is some missing parameter, please include" +
+          "genres, topics, childs and deep",
+      });
+      return;
+    }
+
+    const climax = Math.trunc((deep - 1) / 2);
+    const resolution = Math.trunc(deep - 1);
+    let risingAction = -1;
+    let fallingAction = -1;
+
+    if (deep >= 5) {
+      risingAction = Math.trunc((deep - 1) / 4);
+      fallingAction = Math.trunc(((deep - 1) / 4) * 3);
+    }
+
+    let initialMessage = `
+  Consider a data model named 'twist' which represent a fraction of a story and only consist of a parameter 'title' of no more than 80 chars and a parameter 'body' of no more than 1200 chars.
+  I need to create an interactive story where readers will be able to choose how to continue the narrative. The main objective of each twist is to convince the reader to keep reading.
+  Please generate a twist containing the exposition for a story of the following genres: ${genres}
+  And the following topics: ${topics}
+  I need you todeliver a JSON that has a key '0' containing the generated twist.`;
+
+    if (extra != undefined) {
+      initialMessage += `Please also consider the following: 
+    ${extra}`;
+    }
+
+    const initialConv: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+      {
+        role: "system",
+        content: "You are a helpful assistant designed to output JSON.",
+      },
+      {
+        role: "user",
+        content: initialMessage,
+      },
+    ];
+
+    const completion = await openai.chat.completions.create({
+      model: gptModel,
+      response_format: {type: "json_object"},
+      messages: initialConv,
+    });
+
+    const initialResponse = completion.choices[0].message;
+    const rootStoryAsJson = JSON.parse(initialResponse.content ?? "");
+
+    storyJson = {...storyJson, ...rootStoryAsJson};
+
+    const secondConv: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+      ...initialConv,
+      initialResponse,
+    ];
+
+    await generateTwists(childs ?? 0, deep ?? 0, 1, "0", secondConv);
+
+    response.send(storyJson);
+
+    // eslint-disable-next-line require-jsdoc
+    async function generateTwists(
+      childs: number,
+      maxDeep: number,
+      currentDeep: number,
+      currentTwist: string,
+      currentConversation: OpenAI.Chat.Completions.ChatCompletionMessageParam[]
+    ) {
+      const keys: string[] = [];
+      gptCalls++;
+      console.log("aqui los calls", gptCalls);
+      for (let x = 0; x < childs; x++) {
+        keys.push(`"${currentTwist}${x.toString()}"`);
+      }
+      const keysString = keys.join(", ");
+      let newMessage = `For twist ${currentTwist} please generate ${childs} more twists, each of them will represent a different way to continue twist ${currentTwist}.
+I need you todeliver a JSON that has the keys ${keysString}, each containing a twist`;
+
+      if (currentDeep == risingAction) {
+        console.log("Aqui el Rising Action con current deep: ", currentDeep);
+        newMessage = `For twist ${currentTwist} please generate ${childs} more twists, each of them will represent a different way to continue twist ${currentTwist}. Please include the rising action of the story on this twists.
+I need you todeliver a JSON that has the keys ${keysString}, each containing a twist`;
+      }
+      if (currentDeep == climax) {
+        console.log("Aqui el climax con current deep: ", currentDeep);
+        newMessage = `For twist ${currentTwist} please generate ${childs} more twists, each of them will represent a different way to continue twist ${currentTwist}. Please include the climax of the story on this twists.
+I need you todeliver a JSON that has the keys ${keysString}, each containing a twist`;
+      }
+      if (currentDeep == fallingAction) {
+        console.log("Aqui el fallingAction con current deep: ", currentDeep);
+        newMessage = `For twist ${currentTwist} please generate ${childs} more twists, each of them will represent a different way to continue twist ${currentTwist}. Please include the falling action of the story on this twists.
+I need you todeliver a JSON that has the keys ${keysString}, each containing a twist`;
+      }
+      if (currentDeep == resolution) {
+        console.log("Aqui el resolution con current deep: ", currentDeep);
+        newMessage = `For twist ${currentTwist} please generate ${childs} more twists, each of them will represent a different way to continue twist ${currentTwist}. Please include the resolution of the story on this twists.
+I need you todeliver a JSON that has the keys ${keysString}, each containing a twist`;
+      }
+
+      const newConv: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+        ...currentConversation,
+        {
+          role: "user",
+          content: newMessage,
+        },
+      ];
+
+      // console.log("Aqui la conv: ", newConv);
+
+      const newCompletion = await openai.chat.completions.create({
+        model: gptModel,
+        response_format: {type: "json_object"},
+        messages: newConv,
+      });
+
+      const newResponse = newCompletion.choices[0].message;
+      const twistsAsJson = JSON.parse(newResponse.content ?? "");
+      storyJson = {...storyJson, ...twistsAsJson};
+
+      const finalConv: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+        ...newConv,
+        newResponse,
+      ];
+
+      if (currentDeep < maxDeep - 1) {
+        const promiseArray = [];
+        for (let x = 0; x < childs; x++) {
+          let newChilds = 2;
+          if (currentDeep > 3) {
+            newChilds = 1;
+          }
+          promiseArray.push(
+            generateTwists(
+              newChilds,
               maxDeep,
               currentDeep + 1,
               currentTwist + x.toString(),
