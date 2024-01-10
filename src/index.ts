0a291a57c409c4b59cc3480d8b1cd54837bc1617
Rick <ricardohs@ricardohs.com>
2024-01-09 20:57:44 -0600

    added extra features

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,14 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as functions from "firebase-functions";
-import * as logger from "firebase-functions/logger";
 import OpenAI from "openai";
 import axios from "axios";
 
 const gptModel = "gpt-3.5-turbo-1106";
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+interface LooseObject {
+  [key: string]: any;
+}
 
 export const gptJsonMode = onRequest(
   {secrets: ["OPENAI_API_KEY"]},
@@ -153,6 +151,13 @@ export const generateStory = onRequest(
   }
 );
 
+/**
+ * Uploads an story to Story3.
+ *
+ * @param {Object} body - JSON of the story containing the a key for each of the twists.
+ * @returns {Object} - Returns a JSON containing the hashIdsMap of all of the uploaded twists.
+ */
+
 export const uploadStory = onRequest(
   {secrets: ["STORY3TOKEN"]},
   async (request, response) => {
@@ -192,6 +197,11 @@ export const uploadStory = onRequest(
   }
 );
 
+/**
+ * Sends to analyze the twists of all the provided hashIds.
+ *
+ * @param {Object} body - JSON of the hashIds.
+ */
 export const publishStory = onRequest(
   {secrets: ["STORY3TOKEN"]},
   async (request, response) => {
@@ -373,8 +383,9 @@ export const generateStoryV3 = functions
     });
 
     const body = request.body;
-    const genres: string | undefined = body.genres;
-    const topics: string | undefined = body.topics;
+    // const genres: string | undefined = body.genres;
+    // const topics: string | undefined = body.topics;
+    const instructions: string | undefined = body.instructions;
     const childs: number | undefined = body.childs;
     const deep: number = body.deep ?? 0;
     const extra: string | undefined = body.extra;
@@ -383,8 +394,8 @@ export const generateStoryV3 = functions
     let gptCalls = 0;
 
     if (
-      genres == undefined ||
-      topics == undefined ||
+      // genres == undefined ||
+      // topics == undefined ||
       childs == undefined ||
       deep == 0
     ) {
@@ -406,12 +417,10 @@ export const generateStoryV3 = functions
     //   fallingAction = Math.trunc(((deep - 1) / 4) * 3);
     // }
 
-    let initialMessage = `
-  Consider a data model named 'twist' which represent a fraction of a story and only consist of a parameter 'title' of no more than 80 chars and a parameter 'body' of no more than 1200 chars.
-  I need to create an interactive story where readers will be able to choose how to continue the narrative.
-  Please generate a twist containing the exposition for a story of the following genres: ${genres}
-  And the following topics: ${topics}
-  I need you to provide a JSON object with a key '0', which should contain the twist represented by a nested JSON object with the keys 'title' and 'body.`;
+    let initialMessage =
+      "Consider a data model named 'twist' which represent a fraction of a story and only consist of a parameter 'title' of no more than 80 chars and a parameter 'body' of no more than 1200 chars." +
+      instructions +
+      "I need you to provide a JSON object with a key '0', which should contain a twist with the exposition of the story, represented by a nested JSON object with the keys 'title' and 'body.";
 
     if (extra != undefined) {
       initialMessage += `Please also consider the following: 
@@ -541,3 +550,275 @@ I need you todeliver a JSON that has the keys ${keysString}, each containing a t
       }
     }
   });
+
+export const generateLinearRawStory = functions
+  .runWith({
+    timeoutSeconds: 540,
+    secrets: ["OPENAI_API_KEY"],
+  })
+  .https.onRequest(async (request, response) => {
+    const body = request.body;
+    const instructions: string | undefined = body.instructions;
+
+    if (instructions == undefined) {
+      response.status(400).json({
+        message: "There is some missing parameter, please include instructions",
+      });
+      return;
+    }
+
+    const rawStoryJson = {title: "", story: ""};
+
+    const story = await simpleNonJSONPromt(
+      instructions,
+      process.env.OPENAI_API_KEY ?? ""
+    );
+
+    rawStoryJson.story = story;
+
+    const title = await simpleNonJSONPromt(
+      "Please generate a title for this story:\n" + story,
+      process.env.OPENAI_API_KEY ?? ""
+    );
+
+    rawStoryJson.title = title;
+
+    response.send(rawStoryJson);
+  });
+
+export const formatRawStory = functions
+  .runWith({
+    timeoutSeconds: 540,
+    secrets: ["OPENAI_API_KEY"],
+  })
+  .https.onRequest(async (request, response) => {
+    const body = request.body;
+    const story: string | undefined = body.story;
+    const title: string | undefined = body.title;
+
+    if (story == undefined) {
+      response.status(400).json({
+        message: "There is some missing parameter, please include story",
+      });
+      return;
+    }
+
+    let storyJson = {
+      "0": {title: ""},
+    };
+    let currentKey = "";
+    const splittedStory = story.split("\n\n");
+
+    console.log(splittedStory);
+
+    for (const value of splittedStory) {
+      const message =
+        "Please provide a JSON with a 'title' key including a suggested title of no more than 80 characters for this paraghraph: \n" +
+        value;
+
+      const contentAsJson = await simpleJSONPromt(
+        message,
+        process.env.OPENAI_API_KEY ?? ""
+      );
+
+      const title = contentAsJson.title;
+
+      currentKey += "0";
+      storyJson = {
+        ...storyJson,
+        ...{[currentKey]: {title: title, body: value}},
+      };
+    }
+
+    storyJson["0"]["title"] = title ?? "";
+
+    response.send(storyJson);
+  });
+
+/**
+ * Generates an story with only 3 branches.
+ *
+ * @param {string} instructions - How do you require the story to be generated.
+ * @param {string} instructionsA - Extra instructions for story branch A.
+ * @param {string} instructionsB - Extra instructions for story branch B.
+ * @param {string} instructionsC - Extra instructions for story branch C.
+ * @returns {Object} - Returns a JSON containing the generated story, ready to be used with the 'uploadStory' endpoint.
+ */
+
+export const generateSemiLinearStory = functions
+  .runWith({
+    timeoutSeconds: 540,
+    secrets: ["OPENAI_API_KEY"],
+  })
+  .https.onRequest(async (request, response) => {
+    const body = request.body;
+    const instructions: string | undefined = body.instructions;
+    const instructionsA: string | undefined = body.instructionsA;
+    const instructionsB: string | undefined = body.instructionsB;
+    const instructionsC: string | undefined = body.instructionsC;
+
+    if (instructions == undefined) {
+      response.status(400).json({
+        message: "There is some missing parameter, please include instructions",
+      });
+      return;
+    }
+
+    const story = await simpleNonJSONPromt(
+      "Generate a story about " + instructions + instructionsA ?? "",
+      process.env.OPENAI_API_KEY ?? ""
+    );
+
+    console.log(story);
+
+    const mainTitle = await titleGenerator(
+      story,
+      process.env.OPENAI_API_KEY ?? ""
+    );
+
+    const splittedStory: string[] = story.split("\n\n");
+
+    const storyIntroArray: string[] = splittedStory.slice(0, 2);
+    const storyEndArray: string[] = splittedStory.slice(
+      2,
+      splittedStory.length
+    );
+
+    console.log(storyEndArray);
+
+    const storyIntroString: string = storyIntroArray.join("\n\n");
+    const alt1 = await simpleNonJSONPromt(
+      "Continue this story about " +
+        instructions +
+        " " +
+        (instructionsB ?? "") +
+        ": \n" +
+        storyIntroString,
+      process.env.OPENAI_API_KEY ?? ""
+    );
+    const alt1Array: string[] = alt1.split("\n\n");
+
+    const alt2 = await simpleNonJSONPromt(
+      "Continue this story about " +
+        instructions +
+        " " +
+        (instructionsC ?? "") +
+        ": \n" +
+        storyIntroString,
+      process.env.OPENAI_API_KEY ?? ""
+    );
+    const alt2Array: string[] = alt2.split("\n\n");
+
+    let storyJson: LooseObject = {};
+
+    let currentKey = "";
+
+    for (const value of storyIntroArray) {
+      currentKey += "0";
+      storyJson = {...storyJson, [currentKey]: {body: value}};
+    }
+
+    let baseKey0 = currentKey;
+    let baseKey1 = currentKey;
+    let baseKey2 = currentKey;
+
+    for (const value of storyEndArray) {
+      if (value.length > 20) {
+        baseKey0 += "0";
+        storyJson = {...storyJson, [baseKey0]: {body: value}};
+      }
+    }
+    for (const value of alt1Array) {
+      if (value.length > 20) {
+        baseKey1 += "1";
+        storyJson = {...storyJson, [baseKey1]: {body: value}};
+      }
+    }
+    for (const value of alt2Array) {
+      if (value.length > 20) {
+        baseKey2 += "2";
+        storyJson = {...storyJson, [baseKey2]: {body: value}};
+      }
+    }
+
+    for (const key in storyJson) {
+      if (key != null) {
+        const value = storyJson[key]["body"];
+        const title: string = await titleGenerator(
+          value,
+          process.env.OPENAI_API_KEY ?? ""
+        );
+
+        storyJson[key]["title"] = title;
+      }
+    }
+
+    storyJson["0"]["title"] = mainTitle;
+
+    response.send(storyJson);
+  });
+
+// eslint-disable-next-line require-jsdoc
+async function simpleNonJSONPromt(
+  message: string,
+  apiKey: string
+): Promise<string> {
+  const openai = new OpenAI({
+    apiKey: apiKey,
+  });
+  const initialConv: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {
+      role: "user",
+      content: message,
+    },
+  ];
+  const completion = await openai.chat.completions.create({
+    model: gptModel,
+    messages: initialConv,
+  });
+
+  const result = completion.choices[0].message.content;
+
+  return result ?? "";
+}
+// eslint-disable-next-line require-jsdoc
+async function simpleJSONPromt(
+  message: string,
+  apiKey: string
+): Promise<LooseObject> {
+  const openai = new OpenAI({
+    apiKey: apiKey,
+  });
+
+  const initialConv: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {
+      role: "system",
+      content: "You are a helpful assistant designed to output JSON.",
+    },
+    {
+      role: "user",
+      content: message,
+    },
+  ];
+  const completion = await openai.chat.completions.create({
+    model: gptModel,
+    response_format: {type: "json_object"},
+    messages: initialConv,
+  });
+
+  const responseContent = completion.choices[0].message.content;
+  const contentAsJson = JSON.parse(responseContent ?? "");
+
+  return contentAsJson;
+}
+
+// eslint-disable-next-line require-jsdoc
+async function titleGenerator(body: string, apiKey: string): Promise<string> {
+  const message =
+    "Please provide a JSON with a 'title' key including an engaging title, of no more than 60 characters, for this story: \n" +
+    body;
+
+  const contentAsJson = await simpleJSONPromt(message, apiKey);
+
+  return contentAsJson.title;
+}
